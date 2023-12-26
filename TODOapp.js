e4b0f1be7e2873806a514c3dbb6b8c97a4d460ec
Kai <kaitomo51199@gmail.com>
2023-12-26 18:25:46 +0900

    appdate

--- TODOapp.js
+++ TODOapp.js
@@ -12,10 +12,11 @@ if (todos) {
 }
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    add();
+    event.preventDefault(); //フォームをサブミットしたときのリロードをしないようにする
+    add();//呼び出し元
 })
 
+//入力された値をリストに追加
 function add(todo) {
     let todoText = input.value;
 
@@ -23,7 +24,8 @@ function add(todo) {
         todoText = todo.text;
     }
 
-    if (todoText) {
+    if (todoText.length > 0) //0文字以上の値が入力された場合true (length>0)はなくても良い暗黙的型変換
+    {
         const li = document.createElement("li");
         li.innerText = todoText;
         li.classList.add("list-group-item");
@@ -31,29 +33,36 @@ function add(todo) {
         if (todo && todo.completed) {
             li.classList.add("text-decoration-line-through")
         }
-
+        //右クリックで削除
         li.addEventListener("contextmenu", function (event) {
             event.preventDefault();
             li.remove();
             saveData();
         });
-
+        //左クリックで取り消し線をつける
         li.addEventListener("click", function () {
             li.classList.toggle
                 ("text-decoration-line-through")//toggleなければつける。あれば消す
             saveData();
         });
 
-        ul.appendChild(li);
-        input.value = "";
+        ul.appendChild(li);//ulタグの子供としてタグ追加できる
+        input.value = "";//入力フォームを空にする
         saveData();
     }
 }
 
 //TODOの記録
 function saveData() {
-    const lists = document.querySelectorAll("li");
+    const lists = document.querySelectorAll("li");//liタグをすべて取ってくる
     let todos = [];
+
+    //例:forEach
+    //const array = [1,2,3]
+    //array.forEach(value=>{
+    //console.log(value * 2);
+    //});2.4.6
+
     lists.forEach(list => {
         //オブジェクト
         let todo = {
